96bd0ecea7124fdc3087f7065950349f6460febb
sh4ck1et0n <tchen358@gmail.com>
2024-05-13 01:59:27 -0400

    the style isnt working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,19 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main >
-      <div className={styles.description}>
-        <div className={styles.basicDiv}>
-          <h1>Tyler's Resume</h1>
-        </div>
-        <div className={styles.basicDiv}>
-          <h2> 
-            <Link href="/TylerChenowethResumeOct2023.pdf">
-            My Resume 
-            </Link>
-          </h2>
-        </div>
-        <div className={styles.basicDiv}>
-          <p>Wesbite under construction</p>
-        </div>
+    <main>
+      <div className={styles.basicDiv}>
+        <h1>Tyler's Resume</h1>
+      </div>
+      <div className={styles.basicDiv}>
+        <h2> 
+          <Link href="/TylerChenowethResumeOct2023.pdf">
+          My Resume 
+          </Link>
+        </h2>
+      </div>
+      <div className={styles.basicDiv}>
+        <p>Wesbite under construction</p>
       </div>
     </main>
   );
