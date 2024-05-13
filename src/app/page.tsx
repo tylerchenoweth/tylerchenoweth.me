87da7326c8930e90fda27edf523f60ffed8e67d8
sh4ck1et0n <tchen358@gmail.com>
2024-05-12 23:37:28 -0400

    changed width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1>Tyler's Resume</h1>
       <embed 
         src="./TylerChenowethResumeOct2023.pdf" 
         type="application/pdf" 
         width="100%" 
-        height="100%" 
+        height="700px" 
       />
     </main>
   );
