b0fa5e4f892d2d55912f4aa8a30c8dc38b1658a9
sh4ck1et0n <tchen358@gmail.com>
2024-05-13 22:29:17 -0400

    added a background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,15 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.basicDiv}>
         <h1>Hi, I am Tyler Chenoweth</h1>
-      </div>
-      <div className={styles.basicDiv}>
         <p>
           I am currently building this website using Next.js to show my resume and portfolio.
         </p>
         <p>
           In the meantime you can find my resume by clicking the link below.
         </p>
-      </div>
-      <div className={styles.basicDiv}>
         <h2> 
         My Resume:&nbsp;
           <Link href="/TylerChenowethResumeOct2023.pdf">
---
+++ src/app/peekaboo/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import Link from 'next/link';
+
+export default function Peekaboo() {
+  return (
+    <main>
+      <div>
+        <h1>Were testing this</h1>
+      </div>
+    </main>
+  );
+}
