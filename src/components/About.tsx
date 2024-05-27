8f2d39a9b02d0ff2096944ce18f165238e4eaed4
sh4ck1et0n <tchen358@gmail.com>
2024-05-27 13:57:14 -0400

    modified the gradients and the hover gradient

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,31 +8,30 @@ export default function About() {
     return (
         <div id="about" className={styles.aboutCard} >
             <div className={styles.imgDiv}>
-                <Image src="/gengar.png" alt="Logo" width={300} height={300}/>
+                <Image src="/gengar.png" alt="Logo" width={300} height={300} />
             </div>
             <div className={styles.detailsDiv}>
                 <div className={styles.detailsInnerDiv}>
                     <h1>About</h1>
+                    <hr></hr>
                     <p>Just pay me to build cool shit and dont ask any questions</p>
                 </div>
-                
-                    <Link className={styles.resumeLink} href="/TylerChenowethResumeMay2024.pdf">
-                        <div >
-                            View Resume
-                        </div>
-                    </Link>
-                
+
+                <Link className={styles.resumeLink} href="/TylerChenowethResumeMay2024.pdf">
+                    <h1>View Resume</h1>
+                </Link>
+
             </div>
 
-      
-                <div className={styles.container}>
-                    <div className={styles.content}>
-                        <p>Container with gradient border using border-image.</p>
-                    </div>
+            {/* This was a test div for the gradient border */}
+            {/* <div className={styles.container}>
+                <div className={styles.content}>
+                    <p>Container with gradient border using border-image.</p>
                 </div>
-            
-            
-            
+            </div> */}
+
+
+
         </div>
     )
 }
