ae9e235358a45366ecfd6b6f5c736152484ef89c
sh4ck1et0n <tchen358@gmail.com>
2024-05-27 19:29:08 -0400

    forgot to add these with the icons commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+import Icons from 'unplugin-icons/webpack';
+
+const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      Icons({
+        autoInstall: true,
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
