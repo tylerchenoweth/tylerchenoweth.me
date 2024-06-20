30505c8ef4f8937c271a0b685d21e5e129c89dac
sh4ck1et0n <tchen358@gmail.com>
2024-06-19 23:55:16 -0400

    put the svgs all on one line

--- src/components/Cooper.tsx
+++ src/components/Cooper.tsx
@@ -14,48 +14,19 @@ export function CooperSVG() {
                 /> */}
 
                 {/* head */}
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-                        
-                    "
-                    style={{fill:'white', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points = "10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180" style={{fill:'white', stroke:'black', strokeWidth: 15 }} />
 
                 {/* left eye */}
-                <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none'/>
+                <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none' />
+                
                 {/* right eye */}
                 <ellipse cx="723" cy="413" r="15" rx="25" ry="29" fill="black" stroke='none' />
 
                 {/* nose */}
-                <polygon points=
-                "
-                    851,562
-                    797,524
-                    778,554
-                    801,590
-                    851,562
-                "
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="851,562 797,524 778,554 801,590 851,562" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* mouth */}
-                <polygon points=
-                "
-                    491,634
-                    703,654
-                "
-
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="491,634 703,654" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* whiskers left */}
                 <line x1="411" y1="532"  x2="271" y2="502" style={{stroke:'black', strokeWidth:'10'}} />
@@ -85,45 +56,9 @@ export function CooperNewDesign() {
                 /> */}
 
                 {/* head */}
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-                        
-                    "
-                    style={{fill:'white', stroke:'none', strokeWidth: 15 }}
-                />
+                <polygon points = "10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180" style={{fill:'white', stroke:'none', strokeWidth: 15 }} />
 
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-
-                        567,731
-                        861,562
-                        601,377 
-                        520,106
-                        404,386
-                        281,378
-                        215,92
-                        10,525
-                    "
-                    style={{fill:'none', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points = "10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180 567,731861,562 601,377 520,106404,386 281,378 215,92 10,525" style={{fill:'none', stroke:'black', strokeWidth: 15 }} />
 
                 {/* left eye */}
                 <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none'/>
@@ -131,26 +66,10 @@ export function CooperNewDesign() {
                 <ellipse cx="723" cy="413" r="15" rx="25" ry="29" fill="black" stroke='none' />
 
                 {/* nose */}
-                <polygon points=
-                "
-                    851,562
-                    797,524
-                    778,554
-                    801,590
-                    851,562
-                "
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="851,562 797,524 778,554 801,590 851,562" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* mouth */}
-                <polygon points=
-                "
-                    491,634
-                    703,654
-                "
-
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="491,634 703,654" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* whiskers left */}
                 <line x1="411" y1="532"  x2="271" y2="502" style={{stroke:'black', strokeWidth:'10'}} />
@@ -185,46 +104,10 @@ export function CooperSvgNoBottom(cooperBody, cooperSecondBody, cooperWhiskers) 
     return (
         <svg width="600" height="600" viewBox="0 0 1200 1200">
 
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-                        
-                    "
-                    style={{...cooperSecondBody.cooperSecondBody}}
-                />
+                <polygon points="10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180" style={{...cooperSecondBody.cooperSecondBody}} />
 
                 {/* head */}
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-
-                        567,731
-                        861,562
-                        601,377 
-                        520,106
-                        404,386
-                        281,378
-                        215,92
-                        10,525
-                    "
-                    style={{fill:'none', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180 567,731 861,562 601,377 520,106 404,386 281,378 215,92 10,525" style={{fill:'none', stroke:'black', strokeWidth: 15 }} />
 
                 {/* left eye */}
                 <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none'/>
@@ -232,26 +115,10 @@ export function CooperSvgNoBottom(cooperBody, cooperSecondBody, cooperWhiskers) 
                 <ellipse cx="723" cy="413" r="15" rx="25" ry="29" fill="black" stroke='none' />
 
                 {/* nose */}
-                <polygon points=
-                "
-                    851,562
-                    797,524
-                    778,554
-                    801,590
-                    851,562
-                "
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="851,562 797,524 778,554 801,590 851,562" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* mouth */}
-                <polygon points=
-                "
-                    491,634
-                    703,654
-                "
-
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="491,634 703,654" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* whiskers left */}
                 <line x1="411" y1="532"  x2="271" y2="502" style={{...cooperWhiskers.cooperWhiskers}} />
@@ -275,21 +142,7 @@ export function CooperSvgDiamond(cooperBody, cooperSecondBody, cooperWhiskers) {
         <svg width="600" height="600" viewBox="0 0 1200 1200">
 
                 {/* head */}
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180 
-                        
-                    "
-                    style={{ ...cooperBody.cooperBody }}
-                />
+                <polygon points="10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180" style={{ ...cooperBody.cooperBody }} />
 
                 {/* left eye */}
                 <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none'/>
@@ -297,26 +150,10 @@ export function CooperSvgDiamond(cooperBody, cooperSecondBody, cooperWhiskers) {
                 <ellipse cx="723" cy="413" r="15" rx="25" ry="29" fill="black" stroke='none' />
 
                 {/* nose */}
-                <polygon points=
-                "
-                    851,562
-                    797,524
-                    778,554
-                    801,590
-                    851,562
-                "
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="851,562 797,524 778,554 801,590 851,562" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* mouth */}
-                <polygon points=
-                "
-                    491,634
-                    703,654
-                "
-
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="491,634 703,654" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
 
                 
@@ -342,22 +179,7 @@ export function CooperSvgSquare(cooperBody, cooperSecondBody, cooperWhiskers) {
         <svg width="600" height="600" viewBox="0 0 1200 1200">
 
                 {/* head */}
-                <polygon points = 
-                    "
-                        10,525 
-                        215,92 
-                        281,378 
-                        404,386 
-                        520,106 
-                        601,377 
-                        861,562 
-                        567,731 
-                        394,1180
-                        10,1180 
-                        
-                    "
-                    style={{...cooperBody.cooperBody}}
-                />
+                <polygon points="10,525 215,92 281,378 404,386 520,106 601,377 861,562 567,731 394,1180 10,1180" style={{...cooperBody.cooperBody}} />
 
                 {/* left eye */}
                 <ellipse cx="486" cy="435" r="15" rx="25" ry="30" fill="black" stroke='none'/>
@@ -365,26 +187,10 @@ export function CooperSvgSquare(cooperBody, cooperSecondBody, cooperWhiskers) {
                 <ellipse cx="723" cy="413" r="15" rx="25" ry="29" fill="black" stroke='none' />
 
                 {/* nose */}
-                <polygon points=
-                "
-                    851,562
-                    797,524
-                    778,554
-                    801,590
-                    851,562
-                "
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="851,562 797,524 778,554 801,590 851,562" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* mouth */}
-                <polygon points=
-                "
-                    491,634
-                    703,654
-                "
-
-                style={{fill:'black', stroke:'black', strokeWidth: 15 }}
-                />
+                <polygon points="491,634 703,654" style={{fill:'black', stroke:'black', strokeWidth: 15 }} />
 
                 {/* whiskers left */}
                 <line x1="411" y1="532"  x2="271" y2="502" style={{...cooperWhiskers.cooperWhiskers}} />
