eefe53076b18c08ef01cedf813298ee34bf4985a
sh4ck1et0n <tchen358@gmail.com>
2024-08-05 14:37:21 -0400

    the jumbotron is linked to the Home nav button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,58 +22,53 @@ export default function Home() {
 
     
     // This basically works
-    <div >
+    <div>
       
       <Navbar/>
-      <div>
+      <div id="home">
             <div className={`${styles.animationTrail} ${styles.fadecontent}` }>
-                        <div className={styles.JumbotronText}>
-                            <h1>Hi, Im Tyler Chenoweth</h1>
-                            <p>I am a Full-Stack Developer based in Tampa, FL</p>
-                        </div>
-                        <div className={styles.Tree}>
-                            {/* <h1>We're Moving</h1> */}
-                            <svg width="500" height="300" viewBox="0 0 1400 1400">
-                                <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
-                            </svg>
-                        </div>
-                        
-                        <div className={styles.Tree}>
-                            {/* <h1>We're Moving</h1> */}
-                            <svg width="500" height="300" viewBox="0 0 1400 1400">
-                                <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
-                            </svg>
-                        </div>
+                <div className={styles.JumbotronText}>
+                    <h1>Hi, Im Tyler Chenoweth</h1>
+                    <p>I am a Full-Stack Developer based in Tampa, FL</p>
+                </div>
+                <div className={styles.Tree}>
+                    {/* <h1>We're Moving</h1> */}
+                    <svg width="500" height="300" viewBox="0 0 1400 1400">
+                        <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
+                    </svg>
+                </div>
+                
+                
 
-                        <div className={`${styles.Tree} ${styles.Tree2}`}>
-                            {/* <h1>We're Moving</h1> */}
-                            <svg width="500" height="300" viewBox="0 0 1400 1400">
-                                <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
-                            </svg>
-                        </div>
+                <div className={`${styles.Tree} ${styles.Tree2}`}>
+                    {/* <h1>We're Moving</h1> */}
+                    <svg width="500" height="300" viewBox="0 0 1400 1400">
+                        <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
+                    </svg>
+                </div>
 
-                        <div className={`${styles.Tree} ${styles.Tree3}`}>
-                            {/* <h1>We're Moving</h1> */}
-                            <svg width="500" height="300" viewBox="0 0 1400 1400">
-                                <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
-                            </svg>
-                        </div>
+                <div className={`${styles.Tree} ${styles.Tree3}`}>
+                    {/* <h1>We're Moving</h1> */}
+                    <svg width="500" height="300" viewBox="0 0 1400 1400">
+                        <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
+                    </svg>
+                </div>
 
-                        <div className={`${styles.Tree} ${styles.Tree4}`}>
-                            {/* <h1>We're Moving</h1> */}
-                            <svg width="500" height="300" viewBox="0 0 1400 1400">
-                                <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
-                            </svg>
-                        </div>
+                <div className={`${styles.Tree} ${styles.Tree4}`}>
+                    {/* <h1>We're Moving</h1> */}
+                    <svg width="500" height="300" viewBox="0 0 1400 1400">
+                        <polygon points="600,1290 240,1290 360,1060 285,1060 430,790 350,790 485,540 420,540 638,135 860,540 790,540 925,790 845,790 990,1060 915,1060 1035,1290 675,1290 675,1510 600,1510" fill="Green" />
+                    </svg>
+                </div>
 
-        
-                        <div className={styles.trainSvg}>
-                            <img src="./TrainwTender.svg" width={700} height={200} />
-                        </div>
-                        <div className={styles.mountainDiv}>
-                            {/* <img src="./MountainsBackground.svg" width={500} height={200} /> */}
-                            {/* <h1>Fucking fukc</h1> */}
-                        </div>
+
+                <div className={styles.trainSvg}>
+                    <img src="./TrainwTender.svg" width={700} height={200} />
+                </div>
+                <div className={styles.mountainDiv}>
+                    {/* <img src="./MountainsBackground.svg" width={500} height={200} /> */}
+                    {/* <h1>Fucking fukc</h1> */}
+                </div>
                         
                         
                         
