7eda6a62b3f83d49fdb68532d98036762fb7e034
sh4ck1et0n <tchen358@gmail.com>
2024-08-06 00:21:38 -0400

    components for the mountain

---
+++ src/components/ScrollingImage.js
@@ -0,0 +1,8 @@
+// components/ScrollingImage.js
+import styles from './ScrollingImage.module.css';
+
+const ScrollingImage = () => {
+  return <div className={styles.scrollContainer}></div>;
+};
+
+export default ScrollingImage;
